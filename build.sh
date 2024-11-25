783e1a269a1920b995a214022ac5b43d027f2199
lublak <lublak.de@gmail.com>
2024-11-25 23:31:06 +0000

    update build environment

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
-rustup target add wasm32-wasi
+if [ ! -d "binaryen-version_119" ]; then
+    curl -L https://github.com/WebAssembly/binaryen/releases/download/version_119/binaryen-version_119-x86_64-linux.tar.gz | tar zx
+fi
+if [ ! -d "wasm-minimal-protocol" ]; then
+    git clone https://github.com/astrale-sharp/wasm-minimal-protocol
+fi
+cd wasm-minimal-protocol/crates/wasi-stub
+cargo install --path .
+cd ../../..
+rustup target add wasm32-wasip1
 cp README.md typst-package/
 cp LICENSE typst-package/
-cargo build --release --target wasm32-wasi
-wasi-stub -r 0 ./target/wasm32-wasi/release/ctxjs.wasm -o typst-package/ctxjs.wasm
-wasm-opt typst-package/ctxjs.wasm -O3 --enable-bulk-memory -o typst-package/ctxjs.wasm
+cargo build --release --target wasm32-wasip1
+wasi-stub -r 0 ./target/wasm32-wasip1/release/ctxjs.wasm -o typst-package/ctxjs.wasm
+binaryen-version_119/bin/wasm-opt typst-package/ctxjs.wasm -O3 --enable-bulk-memory -o typst-package/ctxjs.wasm 
